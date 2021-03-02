473e64cd991d5c277f5c6910bf63635f72bca7e9
gsidburycrawford <66580946+gsidburycrawford@users.noreply.github.com>
2021-03-02 17:06:20 -0500

    commit

---
+++ Week 2/index.js
@@ -0,0 +1,41 @@
+console.log('hello world');
+
+var myAge = 21;
+var yourAge = 25;
+
+console.log(myAge, yourAge);
+
+const favePizza = 'pepperoni'
+console.log(favePizza);
+
+let x = 7 +7 + 7 //21
+console.log(x);
+
+let coffee = "hot"; //string
+let temp = 67; //number
+let weather = "27"; //string
+let age ="30";
+let itsSunny = false; //boolean
+
+
+console.log(weather + age + temp);
+
+//Challenge 1
+console.log("Do You Need More Coffee?")
+var numCups = readLine();
+if (numCups < 3) {
+    console.log("Yes, I'll take another cup of coffee");
+
+} else {
+    console.log("I think I'm okay for now.");
+}
+
+//Challenge 2
+console.log("Does your car need an oil change?")
+var numMiles = readLine();
+if (numMiles > 500) {
+    console.log("Yes I think I need an oil change.");
+
+} else {
+    console.log("My car doesn't need an oil change yet.");
+}
